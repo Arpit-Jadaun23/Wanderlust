4b8f299bd1c5b197e75e3cb9d6a5a494847b85ba
Arpit Jadaun <jadaunarpit9@gmail.com>
2025-04-24 13:13:47 +0530

    Add route

--- app.js
+++ app.js
@@ -113,6 +113,10 @@ app.use("/", userRouter);
     next(new ExpressError(404, "Page not found!"));
 });*/
 
+app.get("/", (req, res) =>{
+    res.render("index.ejs");
+});
+
 app.use((err, req, res, next)=>{
     let {statusCode = 500, message = "Something went wrong"} = err;
     res.status(statusCode).render("error.ejs", {message});
