8f308bd294efcdcb5c5784a54d13a196a59f5e96
Arpit Jadaun <jadaunarpit9@gmail.com>
2025-05-07 11:39:23 +0530

    fix reviews

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -22,6 +22,6 @@ module.exports.destroyReview = async(req, res)=>{
     await Review.findByIdAndDelete(reviewId);
 
     req.flash("success", "Review Deleted!");
-    res.redirect(`/listings/${id}`);
+    //res.redirect(`/listings/${id}`);
 
 }
